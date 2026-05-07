7cc8c22598d8756221cd9ea20b70bdd729d067d3
SwastiKh <swastikhurana0800@gmail.com>
2026-05-07 21:10:46 +0200

    remaining changes for random env init

--- utils.py
+++ utils.py
@@ -19,7 +19,7 @@ THRESHOLD_DEPLETION=0.32  # 0.4 #deepmind paper- 0.32, human=0.6
 THRESHOLD_RESTORATION=0.2 # 0.0 #deepmind paper- 0.0, human=0.3
 DIRT_SPAWN_PROBABILITY=0.5  # 0.5 #deepmind paper- 0.5, human=0.6
 DELAY_START_OF_DIRT_SPAWNING=50  # 50
-RUN_NAME = "share_clean_rwd_512_steps_random_init"  # "SHARED" or "INEQUITY_AVERSION" or "BASIC_ENV"
+RUN_NAME = "share_clean_rwd_512_steps_random_init_eval_nondeterministic"  # "SHARED" or "INEQUITY_AVERSION" or "BASIC_ENV"
 SHARED_CLEANING_REWARDS = True  # True or False
 SHARED_REWARDS = False  # True or False
 INEQUITY_AVERSION = False  # True or False
